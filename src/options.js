299e6ebffacd11953e35b85b2018259e6bc72666
atsukoba <atsuya_wgp4346@keio.jp>
2019-10-12 06:45:40 +0900

    update option page

---
+++ src/options.js
@@ -0,0 +1,26 @@
+const hsl2rgb = (h, s, l) => {
+  let r, g, b;
+  if(s == 0){
+      r = g = b = l; // achromatic
+  }else{
+    let hue2rgb = function hue2rgb(p, q, t) {
+        if(t < 0) t += 1;
+        if(t > 1) t -= 1;
+        if(t < 1/6) return p + (q - p) * 6 * t;
+        if(t < 1/2) return q;
+        if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+        return p;
+    }
+    let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    let p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+const colorChanger = setInterval(() => {;
+  let rgb = hsl2rgb(Math.random(), 1.0, 0.75);
+  document.getElementById("kamo").style.fill = `rgb(${rgb.join(',')})`;
+}, 1000);
